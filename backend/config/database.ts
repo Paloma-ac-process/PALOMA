c47a827bdc4afe8d25c0e3c6cfd1ca7b896d5ed5
tpellizzari <theo.pellizzari2@ent.univ-lorraine.fr>
2025-07-15 09:18:19 +0200

    switch to mysql & workflows github

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -2,12 +2,16 @@ import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'sqlite',
+  connection: 'mysql',
   connections: {
-    sqlite: {
-      client: 'sqlite3',
+    mysql: {
+      client: 'mysql2',
       connection: {
-        filename: env.get('DB_FILENAME', 'database/database.sqlite'),
+        host: env.get('DB_HOST'),
+        port: env.get('DB_PORT'),
+        user: env.get('DB_USER'),
+        password: env.get('DB_PASSWORD'),
+        database: env.get('DB_DATABASE'),
       },
       migrations: {
         naturalSort: true,
